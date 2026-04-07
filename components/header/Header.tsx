8876813ca1a14011260d1626f727e5097d24d14b
daniel petcov <danielpetcov.work@gmail.com>
2026-04-07 23:19:39 +0300

    made the logo as link

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -12,6 +12,8 @@ import HeaderActionButtons from "./HeaderActionButtons"
 import HeaderLinks from "./HeaderLinks"
 import { useTranslations } from "next-intl"
 
+import { Link } from "@/i18n/navigation"
+
 export default function Header() {
   const router = useRouter()
   const t = useTranslations("Header")
@@ -27,7 +29,9 @@ export default function Header() {
   return (
     <header className="flex w-full items-center border-b px-5 py-4">
       <SidebarTrigger className="mr-4" />
-      <div className="text-2xl font-bold">StudyCards</div>
+      <Link href="/dashboard" className="text-2xl font-bold">
+        StudyCards
+      </Link>
       <div className="mx-4 flex grow items-center justify-end lg:justify-between">
         <HeaderLinks className="hidden lg:block" />
         {/* <InputGroup className="max-w-50">
